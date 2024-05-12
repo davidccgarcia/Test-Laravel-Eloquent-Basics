32a0597b79f1c2473960070d432af362a004f831
David García <ccristhiangarcia@gmail.com>
2024-05-11 20:27:51 -0500

    All test passed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Observers\ProjectObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,4 +11,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
 }
